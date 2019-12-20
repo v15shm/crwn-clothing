4c14350f98a3da656a61343080636b761522b482
v15shm <34422496+v15shm@users.noreply.github.com>
2019-12-20 08:57:55 +0000

    commit 20Dec 2019 - 6.7

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import {Switch, Route } from 'react-router-dom';
 import './App.css';
-import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 import HomePage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
