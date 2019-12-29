da2a7b4674a1eb2f6c2711a88df32eb429c9eefa
v15shm <34422496+v15shm@users.noreply.github.com>
2019-12-29 09:35:56 +0000

    created cartitem and cartitemcount selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
-
+import { selectCartItems } from '../../redux/cart/cart.selectors';
 
 import './cart-dropdown.styles.scss';
 
@@ -18,8 +18,8 @@ const CartDropdown = ({ cartItems }) => (
     </div>
 );
 
-const mapSpateToProps = ({ cart : { cartItems } }) => ({
-    cartItems
+const mapSpateToProps = (state) => ({
+    cartItems: selectCartItems(state)
 });
 
 export default connect(mapSpateToProps)(CartDropdown);
